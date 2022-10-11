24664d1d112913897ef3788dac86bae1ac73f5e5
joice.joseph <joice.joseph@tothenew.com>
2022-10-11 23:17:08 +0530

    Maven Assignment + Doc file with screenshots

---
+++ Assignment7/MavenDemo/src/test/java/org/example/TestMain.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMain {
+
+    @Test
+    public void testMain(){
+        // dummy test - that makes absolutely zero sense.
+        String result = "";
+        Assert.assertEquals(result,"");
+
+    }
+}
